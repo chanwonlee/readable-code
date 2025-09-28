1fdc7f2a3f61e3a813bf1f48426e42c1fb476166
chanwon <clw8679@naver.com>
2025-09-28 09:52:54 +0900

    섹션 5 상속과 조합

--- src/main/java/cleancode/minesweeper/tobe/cell/EmptyCell.java
+++ src/main/java/cleancode/minesweeper/tobe/cell/EmptyCell.java
@@ -1,27 +1,46 @@
 package cleancode.minesweeper.tobe.cell;
 
-public class EmptyCell extends Cell {
+public class EmptyCell implements Cell {
 
     private static final String EMPTY_SIGN = "■";
 
-    @Override
+    private final CellState cellState = CellState.initialize();
+
     public boolean isLandMine() {
         return false;
     }
 
-    @Override
     public boolean hasLandMineCount() {
         return false;
     }
 
-    @Override
     public String getSign() {
-        if (isOpened()) {
+        if (cellState.isOpened()) {
             return EMPTY_SIGN;
         }
-        if (isFlagged) {
+        if (cellState.isFlagged()) {
             return FLAG_SIGN;
         }
         return UNCHECKED_SIGN;
+    }
+
+    @Override
+    public void flag() {
+        cellState.flag();
+    }
+
+    @Override
+    public void open() {
+        cellState.open();
+    }
+
+    @Override
+    public boolean isChecked() {
+        return cellState.isChecked();
+    }
+
+    @Override
+    public boolean isOpened() {
+        return cellState.isOpened();
     }
 }
--- src/main/java/cleancode/minesweeper/tobe/cell/LandMineCell.java
+++ src/main/java/cleancode/minesweeper/tobe/cell/LandMineCell.java
@@ -1,27 +1,47 @@
 package cleancode.minesweeper.tobe.cell;
 
-public class LandMineCell extends Cell {
+public class LandMineCell implements Cell {
 
     private static final String LAND_MINE_SIGN = "☼";
 
-    @Override
+    private final CellState cellState = CellState.initialize();
+
     public boolean isLandMine() {
         return true;
     }
 
-    @Override
     public boolean hasLandMineCount() {
         return false;
     }
 
     @Override
     public String getSign() {
-        if (isOpened()) {
+        if (cellState.isOpened()) {
             return LAND_MINE_SIGN;
         }
-        if (isFlagged) {
+        if (cellState.isFlagged()) {
             return FLAG_SIGN;
         }
         return UNCHECKED_SIGN;
+    }
+
+    @Override
+    public void flag() {
+        cellState.flag();
+    }
+
+    @Override
+    public void open() {
+        cellState.open();
+    }
+
+    @Override
+    public boolean isChecked() {
+        return cellState.isChecked();
+    }
+
+    @Override
+    public boolean isOpened() {
+        return cellState.isOpened();
     }
 }
